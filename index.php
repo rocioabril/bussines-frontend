fae70e35784dd8588d769028ee2fba5a83bc4a14
rocioabril <rocioacelaco@gmail.com>
2022-09-23 14:24:47 +0200

    changes

--- index.php
+++ index.php
@@ -3,15 +3,34 @@
         <section class="py-5 border-bottom" id="features">
             <div class="container px-5 my-5">
                 <div class="row gx-5">
-                    <div class="col-lg-4 mb-5 mb-lg-0">
+
+                <?php
+
+                    if ( have_posts() ) :
+                            while ( have_posts() ) : the_post(); ?>
+
+                        <div class="col-lg-4 mb-5 mb-lg-0">
                         <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-collection"></i></div>
-                        <h2 class="h4 fw-bolder">Featured title</h2>
+                        <h2 class="h4 fw-bolder"><?php the_title() ?></h2>
                         <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
                         <a class="text-decoration-none" href="#!">
                             Call to action
                             <i class="bi bi-arrow-right"></i>
                         </a>
-                    </div>
+                        </div>
+
+                            <?php endwhile;
+
+
+                    else :
+                            echo '<p>There are no posts!</p>';
+
+                    endif;
+
+                ?>
+
+
+                    <!--
                     <div class="col-lg-4 mb-5 mb-lg-0">
                         <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-building"></i></div>
                         <h2 class="h4 fw-bolder">Featured title</h2>
@@ -30,6 +49,7 @@
                             <i class="bi bi-arrow-right"></i>
                         </a>
                     </div>
+                    -->
                 </div>
             </div>
         </section>
