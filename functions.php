021892d0fa8625c771ecd1d5011486a1b922a2cb
rocioabril <rocioacelaco@gmail.com>
2022-09-26 13:37:39 +0200

    add formulario y footer

--- functions.php
+++ functions.php
@@ -4,5 +4,20 @@ add_theme_support("post-thumbnails");
 add_theme_support("menus");
 add_theme_support("widgets");
 
+function registrar_sidebar(){
+    register_sidebar(array(
+     'name' => 'Sidebar del Footer',
+     'id' => 'sidebar-footer',
+     'description' => 'Descripción de footer',
+     'class' => 'sidebar',
+     'before_widget' => '<div id="%1$s" class="widget %2$s">',
+     'after_widget' => '</div>',
+     'before_title' => '<h2 class="widget-title">',
+     'after_title' => '</h2>',
+    ));
+  }
+  add_action( 'widgets_init', 'registrar_sidebar');
+
+
 ?>
 
